3c0f27ffd3d902b2dccf7acbe05bd09dc34a3f65
erikna7 <erikson.saragi@humantechno.co.id>
2024-05-19 17:19:10 +0700

    change to simple resume

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,32 +1,92 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.onload = () => {
+  const nav = document.querySelector('nav');
+  const anchors = document.querySelectorAll('.nav-link');
+  const topAnchor = document.querySelector('.scroll-top');
+  const nav_toggle = document.querySelector('#nav-toggle');
+  const nav_menu = document.getElementById('nav-menu');
+  const navbarHeight = 54;
   
-  const anchors = document.querySelectorAll('.nav-item');
-  const sections = document.querySelectorAll('section');
-
-  function getShouldActiveNav(section_id) {
-    const nav = Object.values(anchors).find((a, i) => {
-      const target = a.querySelector('a').href.split('#')[1]; 
-      return target == section_id;
-    })
-    return nav
+  function openMenu() {
+    console.log('navmenu display: ', nav_menu.style.display);
+    console.log(nav.className);
+    console.log(nav.classList);
+    if (nav_menu.style.display == '') {
+      nav_toggle.style.rotate = '-180deg';
+      nav.classList.remove('close');
+      nav_menu.style.display = 'block';
+      setTimeout(() => {
+        nav.classList.add('open');
+      }, 100)
+    } else if (nav_menu.style.display == 'block') {
+      nav_toggle.style.rotate = '-90deg';
+      nav.classList.remove('open');
+      nav.classList.add('close');
+      setTimeout(() => {
+        nav_menu.style.display = '';
+      }, 500)
+    }
   }
 
-  function handleScroll() {
-    sections.forEach((section) => {
-      const rect = section.getBoundingClientRect();
-      // const isVisible = rect.top < window.innerHeight && rect.bottom  >= 0;
-      const isVisible = parseInt(rect.bottom) <= parseInt(rect.height + 80) && rect.bottom >= 0
-      const navItem = getShouldActiveNav(section.id);
-      if (isVisible) {
-          navItem.classList.add('visited');
+  topAnchor.addEventListener('click', (e) => {
+    e.preventDefault();
+    const targetId = topAnchor.getAttribute('href')
+    const topSection = document.getElementById(targetId.substring(1));
+    scrollToTarget(topSection);
+    
+    anchors.forEach(a => {
+      if (a.getAttribute('href') === targetId) {
+        a.style.backgroundColor = 'var(--grey)';
+        a.style.color = 'var(--tosca-accent)';
       } else {
-        navItem.classList.remove('visited')
+        a.style.removeProperty('background-color');
+        a.style.removeProperty('color');
+      }
+    })
+  })
+
+  function activateAnchor(e) {
+    e.target.style.backgroundColor = 'var(--grey)';
+    e.target.style.color = 'var(--tosca-accent)';
+    const href = e.target.getAttribute('href');
+    for (i of anchors) {
+      if (href != i.getAttribute('href')) {
+        i.style.removeProperty('background-color');
+        i.style.removeProperty('color');
+      }
+    }
+  }
+
+  function scrollToTarget(target) {
+    var targetPosition = target.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({
+      top: targetPosition - navbarHeight,
+      behavior: 'smooth',
+    });
+  }
+
+  anchors.forEach((a) => {
+    a.addEventListener('click', (e) => {
+      e.preventDefault();
+      activateAnchor(e);
+      const targetId = e.target.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+      if (targetSection) {
+        scrollToTarget(targetSection);
       }
     });
-  };
+  });
 
-  window.addEventListener('scroll', handleScroll);
-  window.addEventListener('resize', handleScroll);
-  handleScroll();
-});
+  nav_toggle.addEventListener('click', () => openMenu());
 
+  document.addEventListener('mousemove', (event) => {
+    if (
+      nav_menu.style.display != '' &&
+      event.clientX < nav_menu.getBoundingClientRect().left
+    ) {
+      nav_toggle.style.rotate = '-90deg';
+      setTimeout(() => {
+        nav_menu.style.display = '';
+      }, 200)
+    }
+  })
+};
